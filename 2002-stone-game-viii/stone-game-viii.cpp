fd87ac3e4ef7802d3f6f0c5646e75a5ae6bf98fc
Ansh Shukla <anshshuklavg@gmail.com>
2026-08-24 22:21:36 +0530

    Time: 134 ms (8.35%) | Memory: 103.1 MB (14.50%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+     int solve(int i,vector<int>&prefix,vector<int>&dp){
+        if(i==prefix.size()-1){
+            return prefix[i];
+        }
+        if(dp[i]!=INT_MIN)return dp[i];
+        int take=prefix[i]-solve(i+1,prefix,dp);
+        int skip=solve(i+1,prefix,dp);
+        dp[i] =max(take,skip);
+        return dp[i];
+
+     }
+    int stoneGameVIII(vector<int>& stones) {
+        int n=stones.size();
+        vector<int>dp(n,INT_MIN);
+      //  dp[0]=max(0,stones[0]);
+        vector<int>prefix(n,0);
+        prefix[0]=stones[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+stones[i];
+        }
+        return solve(1,prefix,dp);
+    }
+};
