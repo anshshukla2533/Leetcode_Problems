30c18f703d00eff2dc681af6eff1e2158d030e9b
Ansh Shukla <anshshuklavg@gmail.com>
2026-09-02 12:02:18 +0530

    Time: 6 ms (50.32%) | Memory: 9.8 MB (65.43%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+       string temp=a;
+       int cnt=1;
+       while(temp.length()<b.length()){
+        temp+=a;
+        cnt++;
+       }
+       if(temp.find(b)!=string::npos){
+        return cnt;
+       }
+       temp+=a;
+       if(temp.find(b)!=string::npos){
+        return cnt+1;
+       }
+       return -1;
+    }
+};
